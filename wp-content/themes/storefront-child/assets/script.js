b6333b0fba689cc2eb19cf5ad7bf7fabb3a06424
alex-rey74 <56680869+alex-rey74@users.noreply.github.com>
2021-03-20 19:12:00 +0100

    Logo Navigation

---
+++ wp-content/themes/storefront-child/assets/script.js
@@ -0,0 +1,75 @@
+class ClassWatcher {
+
+    constructor(targetNode, classToWatch, classAddedCallback, classRemovedCallback) {
+        this.targetNode = targetNode
+        this.classToWatch = classToWatch
+        this.classAddedCallback = classAddedCallback
+        this.classRemovedCallback = classRemovedCallback
+        this.observer = null
+        this.lastClassState = targetNode.classList.contains(this.classToWatch)
+
+        this.init()
+    }
+
+    init() {
+        this.observer = new MutationObserver(this.mutationCallback)
+        this.observe()
+    }
+
+    observe() {
+        this.observer.observe(this.targetNode, { attributes: true })
+    }
+
+    disconnect() {
+        this.observer.disconnect()
+    }
+
+    mutationCallback = mutationsList => {
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                let currentClassState = mutation.target.classList.contains(this.classToWatch)
+                if(this.lastClassState !== currentClassState) {
+                    this.lastClassState = currentClassState
+                    if(currentClassState) {
+                        this.classAddedCallback()
+                    }
+                    else {
+                        this.classRemovedCallback()
+                    }
+                }
+            }
+        }
+    }
+}
+
+let flag = false;
+
+let callbackStickyAdd = () => {
+    if(!flag){
+        let menuBar = document.querySelector('.nav-menu');
+        let li = document.createElement('li');
+        let p = document.createElement('img');
+
+        li.className = "logo-float";
+
+        p.src = "http://hemp-selection/wp-content/uploads/2021/03/logo-sans-titre-sans-fond-mini.png";
+        p.alt = "Logo Hemp Selection";
+        p.className = "logo-sticky";
+
+        li.appendChild(p);
+        menuBar.prepend(li)
+        flag = true;
+    }
+}
+
+let callbackStickyRmv = () => {
+    if(flag){
+        document.querySelector('.logo-sticky').remove();
+        flag = false;
+    }
+}
+
+setTimeout(() => {
+    let target = document.querySelector('.sticky-element-placeholder');
+    let classWatcher = new ClassWatcher(target, 'sticky-element-active', callbackStickyAdd, callbackStickyRmv);
+}, 200);
--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -2,8 +2,13 @@
 /**
 ** activation theme
 **/
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_rss' );
+function theme_enqueue_rss() {
+
+    //Ajout des styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'main-style', get_template(). '/style.css');
+
+    //Ajout du js
+    wp_enqueue_script( 'main-script', get_stylesheet_directory_uri() . '/assets/script.js', array(), '1.0.0', true );
 }
